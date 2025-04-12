968e40b21561008ab42f77384b6f2c8bba32e389
Rezishon <rezashahmoradi.81@outlook.com>
2025-04-12 19:23:18 +0330

    fix(FileReader): rename namespace

--- Repository/FileReader.cs
+++ Repository/FileReader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Compilor_In_Csharp.Repository
+namespace Repository
 {
     public class FileHandler
     {

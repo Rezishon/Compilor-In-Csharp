e9f6e2f7c7cb2e2d527e42bd7cd5560235c396e9
rezishon <rezashahmoradi.81@outlook.com>
2025-07-18 14:00:13 +0330

    feat(Repository)[ResultFileManager]: add `ResultFileManager` class

---
+++ Repository/ResultManager/ResultFileManager.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ResultFileManager
+    {
+}

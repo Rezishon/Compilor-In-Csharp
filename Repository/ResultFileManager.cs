6a1904765faa5582a3406edfd530e446c7bb2224
rezishon <rezashahmoradi.81@outlook.com>
2025-07-22 22:21:46 +0330

    feat(Repository)[ResultFileManager]: add `BuildBinaryFile` method
    It will execute command to build the binary file from the result file
    that should be made earlier
    Error handling also done.

--- Repository/ResultFileManager.cs
+++ Repository/ResultFileManager.cs
@@ -36,5 +36,28 @@ namespace Repository
                 Console.WriteLine("An error occurred while building result file: " + ex.Message);
             }
         }
+
+        private static void BuildBinaryFile()
+        {
+            try
+            {
+                // Start the process
+                using Process process = new Process();
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = "go",
+                    Arguments =
+                        $"build -o {EnvManager.GetEnvironmentVariable(EnvironmentKeys.ResultBinaryFileName)} {EnvManager.GetEnvironmentVariable(EnvironmentKeys.ResultFilePath)}",
+                    CreateNoWindow = true, // No separate window
+                };
+                process.Start();
+
+                process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
     }
 }

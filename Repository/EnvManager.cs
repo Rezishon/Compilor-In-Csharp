1f08c45b60af87422e3631d70a909c1ddc13f6b6
rezishon <rezashahmoradi.81@outlook.com>
2025-08-15 22:26:43 +0330

    feat(Repository)[EnvManager]: add create table queries as environment variable

--- Repository/EnvManager.cs
+++ Repository/EnvManager.cs
@@ -41,6 +41,58 @@ namespace Repository
                 $"{GetEnvironmentVariable(EnvironmentKeys.BaseDirectory)}{GetEnvironmentVariable(EnvironmentKeys.ResultBinaryFileName)}"
             );
             #endregion
+            SetEnvironmentVariable(
+                QueryStrings.CreateEnvironmentVariablesTable,
+                @"
+            CREATE TABLE IF NOT EXISTS EnvironmentVariablesTable (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL,
+                Value TEXT 
+            );"
+            );
+
+            SetEnvironmentVariable(
+                QueryStrings.CreateVariablesTable,
+                @"
+            CREATE TABLE IF NOT EXISTS VariablesTable (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL,
+                Type INTEGER NOT NULL,
+                Value TEXT,
+                ScopeIndex INTEGER NOT NULL,
+                FOREIGN KEY (Type) REFERENCES VariableTypesTable(Id)
+            );"
+            );
+
+            SetEnvironmentVariable(
+                QueryStrings.CreateVariableTypesTable,
+                @"
+            CREATE TABLE IF NOT EXISTS VariableTypesTable (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL,
+                SampleValue TEXT
+            );"
+            );
+
+            SetEnvironmentVariable(
+                QueryStrings.CreateKeywordsTable,
+                @"
+            CREATE TABLE IF NOT EXISTS KeywordsTable (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Type INTEGER NOT NULL,
+                Name TEXT NOT NULL,
+                FOREIGN KEY (Type) REFERENCES KeywordTypesTable(Id)
+            );"
+            );
+
+            SetEnvironmentVariable(
+                QueryStrings.CreateKeywordTypesTable,
+                @"
+            CREATE TABLE IF NOT EXISTS KeywordTypesTable (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL,
+            );"
+            );
         }
 
         public static string GetEnvironmentVariable(EnvironmentKeys key)

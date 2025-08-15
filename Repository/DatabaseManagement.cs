764d20999387d4be71234608912ba31d793dca71
rezishon <rezashahmoradi.81@outlook.com>
2025-08-15 23:16:31 +0330

    feat(Repository)[DatabaseManagement]: add database management class

---
+++ Repository/DatabaseManagement.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Repository;
+
+namespace Compiler_In_Csharp.Repository
+{
+    public class DatabaseManagement
+    {
+    }
+}

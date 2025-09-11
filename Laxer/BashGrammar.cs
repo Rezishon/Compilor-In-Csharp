f208fe4be82bf5477d7999a2660275005cd89da5
rezishon <rezashahmoradi.81@outlook.com>
2025-09-11 23:44:58 +0330

    feat(/Laxer)[BashGrammer.cs]: update grammers + add new syntax

--- Laxer/BashGrammar.cs
+++ Laxer/BashGrammar.cs
@@ -14,6 +14,7 @@ namespace Laxer
             #region Terminals
             var Identifier = new IdentifierTerminal("Identifier");
             var Number = new NumberLiteral("Number");
+            var Boolean = new RegexBasedTerminal("Boolean", @"\b(true|false)\b");
             var SingleQuotedString = new StringLiteral(
                 "SingleQuotedString",
                 "'",
@@ -24,55 +25,143 @@ namespace Laxer
                 "\"",
                 StringOptions.AllowsAllEscapes
             );
-            #endregion
-
-            #region KeyWords
-            var Echo = ToTerm("echo");
-            #endregion
-
-            #region Operators
+            var Variable = new RegexBasedTerminal("Variable", @"\$[A-Za-z_][A-Za-z0-9_]*");
             var Equal = ToTerm("=");
+            var Plus = ToTerm("+");
+            var Minus = ToTerm("-");
+            var Star = ToTerm("*");
+            var Slash = ToTerm("/");
+            var EqOp = ToTerm("-eq");
+            var NeOp = ToTerm("-ne");
+            var LtOp = ToTerm("-lt");
+            var GtOp = ToTerm("-gt");
+            var LeOp = ToTerm("-le");
+            var GeOp = ToTerm("-ge");
+            var AndAnd = ToTerm("&&");
+            var OrOr = ToTerm("||");
+            var Bang = ToTerm("!");
+            var If = ToTerm("if");
+            var Then = ToTerm("then");
+            var Fi = ToTerm("fi");
+            var Else = ToTerm("else");
+            var For = ToTerm("for");
+            var In = ToTerm("in");
+            var Do = ToTerm("do");
+            var Done = ToTerm("done");
+            var While = ToTerm("while");
+            var FunctionKeyword = ToTerm("function");
+            var CmdPrefix = ToTerm("cmd->");
+            var LParen = ToTerm("(");
+            var RParen = ToTerm(")");
+            var LBrace = ToTerm("{");
+            var RBrace = ToTerm("}");
             var Semicolon = ToTerm(";");
             var NewLine = new NewLineTerminal("NewLine");
-            var WhiteSpace = ToTerm(" ");
-            #endregion
-
-            #region Comments
             var Comment = new CommentTerminal("Comment", "#", "\n", "\r\n");
-            NonGrammarTerminals.Add(Comment); // Mark comments as non-grammar terminals so they are ignored by the parser
+            NonGrammarTerminals.Add(Comment);
             #endregion
 
-            #region Non-Terminals
-            var Program = new NonTerminal("Program"); // The root of our grammar
-            var StatementList = new NonTerminal("StatementList"); // A sequence of statements
-            var Statement = new NonTerminal("Statement"); // A single command or assignment
+            #region Non-Terminals and Rules
+            // Non-Terminals represent a group of tokens that form a syntactic unit.
+            var Program = new NonTerminal("Program");
+            var Statement = new NonTerminal("Statement");
+            var Expression = new NonTerminal("Expression");
+            var AssignmentStatement = new NonTerminal("AssignmentStatement");
+            var CommandStatement = new NonTerminal("CommandStatement");
+            var IfStatement = new NonTerminal("IfStatement");
+            var WhileStatement = new NonTerminal("WhileStatement");
+            var ForStatement = new NonTerminal("ForStatement");
+            var FunctionDeclaration = new NonTerminal("FunctionDeclaration");
+            var Block = new NonTerminal("Block");
+            var ArgumentList = new NonTerminal("ArgumentList");
+            var Value = new NonTerminal("Value");
 
-            var Command = new NonTerminal("Command"); // Represents a generic command call
-            var CommandArg = new NonTerminal("CommandArg"); // An argument to a command (string, number, identifier)
-            var CommandArgList = new NonTerminal("CommandArgList");
-            var EchoCommand = new NonTerminal("EchoCommand"); // Specifically for the 'echo' command
+            // Define the rules for each non-terminal
 
-            var VariableAssignment = new NonTerminal("VariableAssignment"); // For syntax like `VAR=VALUE`
-            var VariableName = new NonTerminal("VariableName"); // The name part of a variable
-            var VariableValue = new NonTerminal("VariableValue"); // The value part of a variable
-            #endregion
+            // A Value can be a number, boolean, string, or variable.
+            Value.Rule = Number | Boolean | SingleQuotedString | DoubleQuotedString | Variable;
 
-            #region Productions
-            Program.Rule = StatementList | Empty;
-            StatementList.Rule = MakeStarRule(StatementList, Statement);
-            Statement.Rule = EchoCommand | VariableAssignment | Comment;
-            MakeStarRule(CommandArgList, CommandArg);
-            EchoCommand.Rule = Echo + CommandArgList;
-            CommandArg.Rule = SingleQuotedString | DoubleQuotedString | Number | Identifier;
-            VariableAssignment.Rule = VariableName + Equal + VariableValue;
-            VariableName.Rule = Identifier;
-            VariableValue.Rule = SingleQuotedString | DoubleQuotedString | Number | Identifier;
-            Root = Program;
-            #endregion
+            // An Expression can be a value or a more complex arithmetic/logical expression.
+            Expression.Rule =
+                Value
+                | Expression + Plus + Expression
+                | Expression + Minus + Expression
+                | Expression + Star + Expression
+                | Expression + Slash + Expression
+                | Expression + EqOp + Expression
+                | Expression + NeOp + Expression
+                | Expression + LtOp + Expression
+                | Expression + GtOp + Expression
+                | Expression + LeOp + Expression
+                | Expression + GeOp + Expression
+                | Expression + AndAnd + Expression
+                | Expression + OrOr + Expression
+                | Bang + Expression
+                | LParen + Expression + RParen;
 
-            #region Punctuation
-            MarkPunctuation(Semicolon, NewLine, Equal);
-            MarkTransient(Statement, CommandArg, VariableName, VariableValue);
+            // An AssignmentStatement is an Identifier followed by an equal sign and an expression.
+            AssignmentStatement.Rule = Identifier + Equal + Expression;
+
+            // A CommandStatement is a command name (Identifier) followed by a list of arguments.
+            ArgumentList.Rule = MakeStarRule(ArgumentList, ToTerm(" "), Value | Identifier);
+            CommandStatement.Rule = CmdPrefix + Identifier + ArgumentList;
+
+            // An IfStatement has an expression, a `then` block, and an optional `else` block.
+            IfStatement.Rule =
+                If + Expression + Then + Block + Fi
+                | If + Expression + Then + Block + Else + Block + Fi;
+
+            // A WhileStatement has an expression and a `do` block.
+            WhileStatement.Rule = While + Expression + Do + Block + Done;
+
+            // A ForStatement has a variable, `in`, a list of values, and a `do` block.
+            ForStatement.Rule = For + Identifier + In + ArgumentList + Do + Block + Done;
+
+            // A FunctionDeclaration is a `function` keyword (optional), an Identifier, `()`, and a block.
+            FunctionDeclaration.Rule =
+                FunctionKeyword + Identifier + LParen + RParen + Block
+                | Identifier + LParen + RParen + Block;
+
+            // A Block is a set of statements inside curly braces.
+            Block.Rule = LBrace + Program + RBrace;
+
+            // A Statement can be one of the various types defined above, or a command.
+            Statement.Rule =
+                AssignmentStatement
+                | CommandStatement
+                | IfStatement
+                | WhileStatement
+                | ForStatement
+                | FunctionDeclaration;
+
+            // The main Program is a list of statements separated by newlines or semicolons.
+            Program.Rule = MakeStarRule(Program, NewLine | Semicolon, Statement);
+
+            // Set the root of the grammar to the Program.
+            this.Root = Program;
+
+            // Define operator precedence for arithmetic and logical expressions.
+            RegisterOperators(10, Associativity.Left, Plus, Minus);
+            RegisterOperators(20, Associativity.Left, Star, Slash);
+            RegisterOperators(30, Associativity.Left, EqOp, NeOp, LtOp, GtOp, LeOp, GeOp);
+            RegisterOperators(40, Associativity.Left, AndAnd, OrOr);
+            RegisterOperators(50, Associativity.Left, Bang);
+
+            // Mark special tokens for the parser.
+            MarkPunctuation(
+                LParen,
+                RParen,
+                LBrace,
+                RBrace,
+                Semicolon,
+                NewLine,
+                Then,
+                Do,
+                Done,
+                Fi,
+                Else
+            );
+            MarkTransient(Statement, Block, ArgumentList, Value);
             #endregion
         }
     }

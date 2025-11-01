c507750723aac557c5ddae254d3a71c4cedd3924
rezishon <rezashahmoradi.81@outlook.com>
2025-11-02 02:42:58 +0330

    feat(/theUI/src/app)[app.ts]: add editor component logics

--- theUI/src/app/app.ts
+++ theUI/src/app/app.ts
@@ -1,16 +1,41 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
+import { FileManagement } from './services/file-management/file-management';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [MonacoEditorModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
+  private fileManager = inject(FileManagement);
+
+  codeForm = new FormGroup({
+    code: new FormControl(''),
+  });
+
+  //TODO: Does file changed since last load or not
+
   protected readonly title = signal('theUi');
-  editorOptions = { theme: 'vs-dark', language: 'javascript' };
-  code: string = 'function x() {\nconsole.log("Hello world!");\n}';
+
+  editorOptions = { theme: 'vs-dark', language: 'shell' };
   originalCode: string = 'function x() { // TODO }';
+
+  async ngOnInit() {
+    this.code.setValue((await this.fileManager.getFileCode()).code);
+  }
+
+  async onSaveFile() {
+    console.log('hello');
+
+    this.fileManager.updateFile(this.code.value);
+  }
+
+  get code() {
+    return this.codeForm.get('code') as FormControl;
+  }
 }

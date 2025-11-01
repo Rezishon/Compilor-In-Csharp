de049bea29c254ffd5ebd3255ec89dfcec3b2e16
rezishon <rezashahmoradi.81@outlook.com>
2025-11-01 22:56:46 +0330

    feat(/theAPI)[*]: add the API project
    This project is a minimal api of dotnet core 8.

---
+++ theAPI/Program.cs
@@ -0,0 +1,55 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.MapPost(
+            "/uploadfile",
+            (FileUploadRequest request) =>
+            {
+                if (string.IsNullOrEmpty(request.Base64Data))
+                {
+                    return Results.BadRequest("Base64 data is required.");
+                }
+
+                try
+                {
+                    byte[] fileBytes = Convert.FromBase64String(request.Base64Data);
+
+                    var fileName = request.FileName;
+                    File.WriteAllBytes(
+                        "/home/rezishon/Projects/Compilor-In-Csharp/sample.bsh",
+                        fileBytes
+                    );
+
+                    return Results.Ok(
+                        new { Message = $"فایل شما با سایز {fileBytes.Length}بایت ذخیره شد" }
+                    );
+                }
+                catch (FormatException)
+                {
+                    return Results.BadRequest("The provided string is not a valid Base64 format.");
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem(
+                        $"An unexpected error occurred during file processing.\n{ex}",
+                        statusCode: StatusCodes.Status500InternalServerError
+                    );
+                }
+            }
+        );
+
+        app.Run();
+    }
+}
+
+public class FileUploadRequest
+{
+    public required string Base64Data { get; set; }
+    public string FileName { get; set; } = "sample.bsh";
+}

86e9547d6e97524085b545e0595f9fc9b4c10508
rezishon <rezashahmoradi.81@outlook.com>
2025-11-02 02:41:27 +0330

    feat(/theUI/src/app)[app.config.ts]: add service needed providers

--- theUI/src/app/app.config.ts
+++ theUI/src/app/app.config.ts
@@ -8,6 +8,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     importProvidersFrom(MonacoEditorModule),
+    provideHttpClient(),
   ],
 };
